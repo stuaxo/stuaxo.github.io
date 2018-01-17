118444dfb1a52eb4d6c98a520941bb9c7102f903
Benjamin <benjamin@ec-it.com>
2018-01-17 18:38:20 +0000

    bookmarklet fix

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,2 +1,1 @@
-
-javascript:function loadScript(scriptURL){var scriptElem=document.createElement('script');scriptElem.setAttribute('language','JavaScript');scriptElem.setAttribute('src',scriptURL);document.body.appendChild(scriptElem);}loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');loadScript('https://rawgit.com/ojamin/hsbc-statement-to-csv/master/create-csv.js')
+javascript:{function loadScript(scriptURL){var scriptElem=document.createElement('script');scriptElem.setAttribute('language','JavaScript');scriptElem.setAttribute('src',scriptURL);document.body.appendChild(scriptElem);}loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');loadScript('https://rawgit.com/ojamin/hsbc-statement-to-csv/master/create-csv.js')}
